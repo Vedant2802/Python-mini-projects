9d605d7dfc1df68fc00100d0195347dae7951c58
Vedant2802 <avedant981@gmail.com>
2025-06-18 23:32:29 +0530

    dictionary changes in the code

--- python_basics/dictionary.py
+++ python_basics/dictionary.py
@@ -1,14 +1,14 @@
 name  = {'akshat':25 , 'shanky':26 , 'name2':27}
 
 # looping exclusive to dictionaries
-# for i,j in name.items():
-#     print(i)
-#     print(j)
+for i,j in name.items():
+    print(i)
+    print(j)
 
-# for i , j in name.items():
-#     if(i == "akshat"):
-#         print("index is there")
+for i , j in name.items():
+    if(i == "akshat"):
+        print("index is there")
 
 # methods commonly used pop(index) , popitem()
-# y = name.popitem()
-# print(name)
+y = name.popitem()
+print(name)
